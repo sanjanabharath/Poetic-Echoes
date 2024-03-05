e7c1ccd5e206b4c8483fd408e1705794974fed09
sanjanabharath <sanjanabarath@gmail.com>
2024-03-05 21:28:02 +0530

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+
+const Navbar = () => {
+  return (
+    <div>
+        <h3 className='font-extrabold'>DevBlogs</h3>
+        <Button variant="outlined">Outlined</Button>
+    </div>
+  )
+}
+
+export default Navbar

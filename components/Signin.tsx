cf6bb210b0f2daec009883cfa377f6274b398d8b
sanjanabharath <sanjanabarath@gmail.com>
2024-05-01 20:36:44 +0530

    Customized Signin

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -36,7 +36,7 @@ export default function () {
                       redirect: false,
                     });
                     console.log(res);
-                    router.push("/");
+                    router.push("/poems");
                   }}
                   className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
                 >
